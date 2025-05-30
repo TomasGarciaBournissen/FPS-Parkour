fe3aa8f9be2daebbf4322ba12c1a1db0c7f7662a
TomasGarcia <tomasgarciab30@gmail.com>
2025-05-30 10:41:03 -0300

    money

---
+++ Assets/scripts/MercanciaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public MoneyManager moneyManager;
+    public float cost;    // Start is called before the first frame update
+    private void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            moneyManager.UpdateMoney(-cost);
+            Destroy(gameObject);
+            
+        }
+    }
+
+}
---
+++ Assets/scripts/MoneyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float money;
+
+    public void UpdateMoney(float amount) { 
+        if (money + amount < 0)
+        {
+            //frena la compra
+            Debug.Log("Dinero Insuficiente");
+        }
+        else
+        {
+            money += amount;
+        }
+        
+    }
+}
